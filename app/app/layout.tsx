c15e189b7fb9963e22c5bf7cca53e8ab68f1f6b1
Izaque384 <izaqueandrade384@gmail.com>
2026-08-30 10:48:01 -0300

    feat: mount safe client shortcuts

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
+import ClientShortcuts from "./components/ClientShortcuts";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -36,7 +37,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${sora.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}<ClientShortcuts/></body>
     </html>
   );
 }
