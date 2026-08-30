339c0b253afe0f4aebc126809b941d462635958d
Izaque384 <izaqueandrade384@gmail.com>
2026-08-30 10:38:10 -0300

    fix: remove unstable global DOM enhancement layer

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Sora } from "next/font/google";
 import "./globals.css";
-import UxEnhancements from "./components/UxEnhancements";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -37,7 +36,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${sora.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}<UxEnhancements/></body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
