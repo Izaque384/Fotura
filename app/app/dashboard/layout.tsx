1773635f80d133233db6158afa5adef9dfdcab16
Izaque384 <izaqueandrade384@gmail.com>
2026-09-20 09:10:25 -0300

    perf: allow dashboard shell to be prefetched and reused

--- app/app/dashboard/layout.tsx
+++ app/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import DashboardBillingGate from "./DashboardBillingGate";
 
-export const dynamic = "force-dynamic";
 
 export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <DashboardBillingGate>{children}</DashboardBillingGate>;
