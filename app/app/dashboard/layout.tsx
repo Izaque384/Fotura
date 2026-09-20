c001c3acde71ab194bf5f4b635113302e7991ac4
Izaque384 <izaqueandrade384@gmail.com>
2026-09-20 09:08:40 -0300

    refactor: stop rendering storage indicator globally

--- app/app/dashboard/layout.tsx
+++ app/app/dashboard/layout.tsx
@@ -1,8 +1,7 @@
 import DashboardBillingGate from "./DashboardBillingGate";
-import StorageUsageIndicator from "./StorageUsageIndicator";
 
 export const dynamic = "force-dynamic";
 
 export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <DashboardBillingGate>{children}<StorageUsageIndicator/></DashboardBillingGate>;
+  return <DashboardBillingGate>{children}</DashboardBillingGate>;
 }
